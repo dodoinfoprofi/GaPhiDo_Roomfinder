f3c66a55e2b2ab36a11adc8b55d6a0aaa73444f4
PhilippLiermann <44139214+PhilippLiermann@users.noreply.github.com>
2019-03-31 18:28:24 +0200

    alle möglichen tests

--- app/src/main/java/com/example/philippliermann/raumplaner/Activity2.java
+++ app/src/main/java/com/example/philippliermann/raumplaner/Activity2.java
@@ -1,37 +1,87 @@
 package com.example.philippliermann.raumplaner;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import org.apache.commons.codec.binary.Base64;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class Activity2 extends AppCompatActivity {
+
+    private static final String URL = "https://cis.technikum-wien.at/cis/infoterminal/index.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
-        readWebPage();
+
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        readWebPage(textView3);
     }
 
-    public void readWebPage()
+    public void readWebPage(TextView textView)
     {
-        try
+        textView.setText("Tests");
+        RetrieveWebPage page = new RetrieveWebPage();
+        page.execute(URL);
+
+
+        //Timer um 5 sec zu warten
+        long startTime = System.currentTimeMillis();
+        while(true)
         {
-            Document doc = Jsoup.connect("https://cis.technikum-wien.at/cis/infoterminal/index.php").get();
-            String title = doc.title();
-            Element element = doc.body();
-            element.getElementById("content");
-
-
-            System.out.println("Title : " + title);
+            if(System.currentTimeMillis() - startTime > 5000)
+            {
+                break;
+            }
         }
-        catch(Exception e)
+
+        if(page.doc != null)
         {
+            textView.setText(page.doc.title());
 
         }
+        else
+        {
+            textView.setText(page.exception.toString());
+        }
+    }
 
+    private class RetrieveWebPage extends AsyncTask<String, Void, Document> {
+
+        private Exception exception;
+
+        private Document doc;
+
+        @Override
+        protected Document doInBackground(String... strings) {
+            try {
+                String login = "st17b058" + ":" + "Srw6DfC3!";          // Authorization HTTP Header Field
+                String base64login = new String(Base64.encodeBase64(login.getBytes())); // Kodierung
+                // return Jsoup.connect(strings[0]).header("Authorization", "Basic c3QxN2IwNTg6U3J3NkRmQzM=").get();
+                return Jsoup.connect(strings[0]).get();
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Document doc)
+        {
+            this.doc = doc;
+        }
     }
 }
