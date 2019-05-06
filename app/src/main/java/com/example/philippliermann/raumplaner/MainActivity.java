fb7361d6afb2fef5edd08070a0ee264fdf9b1928
Philipp <p.liermann@freenet.de>
2019-05-06 17:17:32 +0200

    Jsoup hinzugefügt

--- app/src/main/java/com/example/philippliermann/raumplaner/MainActivity.java
+++ app/src/main/java/com/example/philippliermann/raumplaner/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.philippliermann.raumplaner;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button getBtn;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         Button hörsaal = (Button) findViewById(R.id.hörsaal);
         Button edv = (Button) findViewById(R.id.edv);
         Button sem = (Button) findViewById(R.id.seminar);
+        getBtn = (Button) findViewById(R.id.getBtn);
+        result = (TextView)findViewById(R.id.result);
+
 
         hörsaal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
                 openactivity4();
             }
         });
+
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWebsie();
+            }
+        });
     }
 
     public void openactivity2(){
@@ -53,5 +76,41 @@ public class MainActivity extends AppCompatActivity {
     public void openactivity4(){
         Intent intent = new Intent(this, Activity4.class);
         startActivity(intent);
+    }
+
+    private void getWebsie(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("https://cis.technikum-wien.at/cis/infoterminal/index.php?raumtyp_kurzbz=EDV&ort_kurzbz=EDV_A2.06&standort_id=").get();
+
+                    String title = doc.title();
+                    //Elements links = doc.select("a[href]");
+                    //builder.append(title).append("\n");
+
+                    /*for (Element link : links) {
+                        builder.append("\n").append("link : ").append(link.attr("href"))
+                                .append("\n").append("Text : ").append(link.text());
+                    }*/
+                    Element content = doc.getElementById("content");
+                    builder.append(content).append("\n");
+                } catch (IOException e){
+                    builder.append("error : ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
+
+
+
     }
 }
