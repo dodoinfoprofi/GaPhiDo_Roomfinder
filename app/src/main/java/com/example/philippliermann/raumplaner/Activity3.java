ea86b2279905070d57e83c1313a742cd9ee25f1d
dodoinfoprofi <mayerhofer.doris@gmail.com>
2019-05-24 12:15:21 +0200

    Es funktioniert und is hübsch!

--- app/src/main/java/com/example/philippliermann/raumplaner/Activity3.java
+++ app/src/main/java/com/example/philippliermann/raumplaner/Activity3.java
@@ -10,6 +10,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
 import java.io.IOException;
 
 public class Activity3 extends AppCompatActivity {
